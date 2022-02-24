21ba794c529979071aa7dad9d17296023158b084
JuanEchague <loxar240@gmail.com>
2022-02-23 21:17:32 -0300

    Primer commit

---
+++ tarjeta-de-contacto/src/main/java/com/juandelsol/tarjetadecontacto/TarjetaDeContactoApplication.java
@@ -0,0 +1,13 @@
+package com.juandelsol.tarjetadecontacto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TarjetaDeContactoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TarjetaDeContactoApplication.class, args);
+	}
+
+}
---
+++ tarjeta-de-contacto/src/test/java/com/juandelsol/tarjetadecontacto/TarjetaDeContactoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.juandelsol.tarjetadecontacto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TarjetaDeContactoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
